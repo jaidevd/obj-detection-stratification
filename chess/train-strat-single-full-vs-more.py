12bac68211261aef6541e28eb8d0e4086bfd98b8
deshpande.jaidev@gmail.com <deshpande.jaidev@gmail.com>
2022-09-28 10:27:56 +0530

    ENH: Initial random stratification on COCO

---
+++ chess/train-strat-single-full-vs-more.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from main import ChessDataset, mAP
+import numpy as np
+import os
+import torch
+from engine import train_one_epoch
+import torchvision as tv
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from tqdm import tqdm
+import utils
+import pandas as pd
+from datetime import datetime
+from sklearn.model_selection import train_test_split
+from warnings import simplefilter
+
+simplefilter("ignore")
+
+op = os.path
+ROOT = "data/train/"
+
+
+# In[2]:
+
+
+ds = ChessDataset(ROOT)
+
+model = tv.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+num_classes = 12
+
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+# replace the pre-trained head with a new one
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+
+# In[3]:
+
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = model.to(device)
+
+
+# In[4]:
+
+
+# Do the stratification here
+df = [
+    (ann["id"].item(), ds.lenc.inverse_transform(ann["labels"].cpu())) for _, ann in ds
+]
+df = pd.DataFrame(df, columns=["id", "labels"])
+df["count"] = df["labels"].apply(len)
+df["labels"] = df["labels"].apply(lambda x: x[0] if len(x) == 1 else x)
+df["labels"] = df["labels"].apply(lambda x: "full" if len(x) == 32 else x)
+singles = df[df["count"] == 1]
+full = df[df['count'] == 32]
+xdf = pd.concat((singles, full), axis=0)
+singles_train, singles_test = map(
+    lambda x: x.index,
+    train_test_split(xdf, stratify=xdf["labels"], train_size=0.6),
+)
+df.drop(singles_train, axis=0, inplace=True)
+df.drop(singles_test, axis=0, inplace=True)
+df["bin"] = pd.cut(df["count"], bins=10, labels=list("ABCDEFGHIJ"))
+
+others_train, others_test = map(
+    lambda x: x.index,
+    train_test_split(df, stratify=df["bin"], train_size=0.6),
+)
+
+train_ix = np.r_[singles_train, others_train]
+test_ix = np.r_[singles_test, others_test]
+
+np.random.shuffle(train_ix)
+np.random.shuffle(test_ix)
+
+ds_train = torch.utils.data.Subset(ds, train_ix)
+ds_test = torch.utils.data.Subset(ds, test_ix)
+
+##
+
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(
+    params, lr=0.0005, momentum=0.9, weight_decay=1e-4, nesterov=True
+)
+
+data_loader_train = torch.utils.data.DataLoader(
+    ds_train, batch_size=8, shuffle=True, num_workers=4, collate_fn=utils.collate_fn
+)
+data_loader_test = torch.utils.data.DataLoader(
+    ds_test, batch_size=1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn
+)
+num_epochs = 20
+
+# lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+for epoch in tqdm(range(num_epochs)):
+    # train for one epoch, printing every 10 iterations
+    train_one_epoch(model, optimizer, data_loader_train, device, epoch, print_freq=0)
+
+torch.save(model, "data/models/strat_single_full.pt")
+
+
+# In[189]:
+
+
+_ = model.eval()
+
+metrics = {}
+for img, ann in tqdm(ds_test):
+    pred = model([img.to(device)])[0]
+    metrics[ann["id"].item()] = mAP(ann, pred, device)
+
+
+metrics = pd.Series(metrics)
+metrics.to_csv(f"data/output/chess_iou_strat_single_full_vs_more_{datetime.now().isoformat()}.csv")
+print(metrics.mean())
---
+++ coco/main.py
@@ -0,0 +1,178 @@
+import torch
+import pandas as pd
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+from torchvision.transforms.functional import (
+    to_tensor,
+    to_pil_image,
+    convert_image_dtype,
+)
+from torchvision.ops import box_iou
+from torchvision.utils import draw_bounding_boxes
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+op = os.path
+
+
+def mAP(ann, pred, device):
+    boxes = pred["boxes"][pred["scores"] > 0.5]
+    labels = pred["labels"][pred["scores"] > 0.5]
+    ap = []
+    cats = ann["labels"].unique()
+    thresh = torch.arange(0.5, 1, 0.05)
+    for cat in cats:
+        y_true = ann["boxes"][ann["labels"] == cat]
+        y_pred = boxes[labels == cat]
+        iou = box_iou(y_pred, y_true.to(device))
+        if len(iou):
+            mask = iou.expand(len(thresh), *iou.shape) > thresh.expand(
+                *iou.shape[::-1], len(thresh)
+            ).T.to(device)
+            tp = mask.sum(-1).sum(-1)
+            fp = (mask.sum(-1) == 0).sum(-1)
+            fn = (mask.sum(1) == 0).sum(-1)
+            _ap = ((tp / (tp + fp + fn)).sum() / 10).cpu()
+        else:
+            _ap = 0
+
+        ap.append(_ap)
+    return np.mean(ap)
+
+
+class COCODataset(torch.utils.data.Dataset):
+    def __init__(self, root, annpath):
+        self._cache = {}
+        self.root = root
+        self.masks = pd.read_json(annpath)
+        self.imgs = np.sort(self.masks["id"].unique())
+        self.lenc = LabelEncoder()
+        self.masks["label"] = self.lenc.fit_transform(self.masks["label"])
+
+    def get_mask(self, idx):
+        df = self.masks[self.masks["id"] == idx][["label", "bbox"]]
+
+        def _bbox_format(x, y, width, height):
+            return x, y, x + width, y + height
+
+        return df.apply(
+            lambda x: (x["label"], *_bbox_format(*x["bbox"])), axis=1
+        ).tolist()
+
+    def draw_bbox(
+        self,
+        idx,
+        model=None,
+        ax=None,
+        device=False,
+        pred_thresh=0.5,
+        show_iou=True,
+        **kwargs,
+    ):
+        img, ann = self[idx]
+        if not device:
+            device = torch.device('cpu')
+        img = img.to(device)
+        labels_true = self.lenc.inverse_transform(ann["labels"])
+        drawn = draw_bounding_boxes(
+            convert_image_dtype(img, torch.uint8),
+            boxes=ann["boxes"],
+            labels=labels_true,
+            colors="green",
+            width=1,
+            font="Ubuntu-R.ttf",
+            font_size=10,
+        )
+        if model is not None:
+            # Add predictions to the drawn model
+            pred = model([img])[0]
+            cutoff = pred['scores'] > pred_thresh
+            for key in ['labels', 'boxes', 'scores']:
+                pred[key] = pred[key][cutoff]
+            labels_pred = self.lenc.inverse_transform(pred["labels"])
+            drawn = draw_bounding_boxes(
+                drawn,
+                boxes=pred["boxes"],
+                labels=labels_pred,
+                colors="red",
+                width=1,
+                font="Ubuntu-R.ttf",
+                font_size=10,
+            )
+        plt.imshow(to_pil_image(drawn))
+        title = f"{idx}"
+        if show_iou:
+            title += f" ({round(mAP(ann, pred, device), 2)})"
+        plt.title(title)
+        plt.show()
+
+    def plot_sample(self, nrows=2, ncols=2, annot=True, idx=None, **kwargs):
+        if idx is None:
+            n_samples = nrows * ncols
+            newix = np.arange(len(self.imgs))
+            np.random.shuffle(newix)
+            idx = self.imgs[newix][:n_samples]
+
+        fig, ax = plt.subplots(nrows, ncols, **kwargs)
+        try:
+            ax = ax.ravel()
+        except AttributeError:
+            ax = [ax]
+        for _ax, im_id in zip(ax, idx):
+            img, ann = self[im_id]
+            img = to_pil_image(img)
+            _ax.imshow(img)
+            labels = self.lenc.inverse_transform([k.item() for k in ann["labels"]])
+            for (box, label) in zip(ann["boxes"], labels):
+                xmin, ymin, xmax, ymax = box
+                _ax.vlines([xmin, xmax], ymax, ymin, colors="g")
+                _ax.hlines([ymax, ymin], xmin, xmax, color="g")
+                if annot:
+                    _ax.text(xmin, ymin, label, {"fontsize": "xx-small"})
+            _ax.set_axis_off()
+
+        plt.tight_layout()
+        plt.show()
+
+    def __getitem__(self, idx):
+        # load images ad masks
+        idx = self.imgs[idx]
+        if idx in self._cache:
+            return self._cache[idx]
+        img_path = op.join(self.root, f"{idx}".rjust(12, "0") + ".jpg")
+        img = Image.open(img_path).convert("RGB")
+        masks = self.get_mask(idx)
+
+        boxes = [k[1:] for k in masks]
+        labels = [k[0] for k in masks]
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        target = {"id": image_id}
+        target["boxes"] = boxes
+        target["labels"] = torch.tensor(labels)
+        target["area"] = area
+
+        img = to_tensor(img)
+        self._cache[idx] = img, target
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+if __name__ == "__main__":
+    ds = COCODataset("data/train", "data/annotations.json")
+    bad_idx = [
+        411832, 44781, 518685, 455135, 323853, 246725, 30932, 114504, 107167, 37863,
+        246382, 1307, 456936, 558137, 416733, 1355, 221245, 281970, 311337, 81177
+    ]
+    model = torch.load("data/models/random.pt").eval().cpu()
+    # ds.draw_bbox(221245, model)
+    for idx in bad_idx:
+        ds.draw_bbox(idx, model)
